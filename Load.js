d09dd99fa2c51947614310f0fb2f4effb5bd63c7
mohamed eskandrane <93823327+mohamed-eskandrane@users.noreply.github.com>
2023-06-17 23:39:22 +0300

    Create Load.js

---
+++ Load.js
@@ -0,0 +1,20 @@
+function ConvertModeToSun(){
+  document.getElementById("Moon").style.display="inline-block";
+  document.getElementById("Sun").style.display="none";
+  document.querySelector(':root').style.setProperty('--FColor', "wheat"); 
+  document.querySelector(':root').style.setProperty('--EColor', "white");
+  document.querySelector(':root').style.setProperty('--loginColor', "whitesmoke"); 
+  document.querySelector(':root').style.setProperty('--FontColor', "#f2a20b"); 
+  document.querySelector(':root').style.setProperty('--Font2Color', "#a53333"); 
+  document.querySelector(':root').style.setProperty('--Font3Color', "#a53333"); 
+} 
+function ConvertModeToMoon(){
+  document.getElementById("Sun").style.display="inline-block";
+  document.getElementById("Moon").style.display="none";
+  document.querySelector(':root').style.setProperty('--FColor', "#141e30"); 
+  document.querySelector(':root').style.setProperty('--EColor', "#243b55");
+  document.querySelector(':root').style.setProperty('--loginColor', "#00000080"); 
+  document.querySelector(':root').style.setProperty('--FontColor', "white"); 
+  document.querySelector(':root').style.setProperty('--Font2Color', "#d3f6f8"); 
+  document.querySelector(':root').style.setProperty('--Font3Color', "black"); 
+}  
